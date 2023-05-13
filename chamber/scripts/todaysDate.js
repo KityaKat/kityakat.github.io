8f30cabb2a5196d850f42d612f93dd6d6d92bfee
KityaKat <77029364+KityaKat@users.noreply.github.com>
2023-05-13 18:04:50 -0500

    Update todaysDate.js

--- chamber/scripts/todaysDate.js
+++ chamber/scripts/todaysDate.js
@@ -1,1 +1,7 @@
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
+const d = new Date();
+let day = weekday[d.getDay()];
+let name = month[d.getMonth()];
+document.write(day + ", " + name + new Date().getDate() +", " + new Date().getFullYear() );
