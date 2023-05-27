240cc8b2e1bf4fc713fa82b5154345d2011817c2
KityaKat <77029364+KityaKat@users.noreply.github.com>
2023-05-27 16:24:35 -0500

    Update todaysDate.js

--- chamber/scripts/todaysDate.js
+++ chamber/scripts/todaysDate.js
@@ -5,3 +5,16 @@ const d = new Date();
 let day = weekday[d.getDay()];
 let name = month[d.getMonth()];
 document.write(day + ", " + name + new Date().getDate() +", " + new Date().getFullYear() );
+
+
+if (day == "Monday") {
+  document.writeln(
+    "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
+  );
+} else if (day == "Tuesday") {
+  document.writeln(
+    "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
+  );
+} else {
+  document.writeln(" ");
+}
